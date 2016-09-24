c7b99ef6dbec982af85eff1ef8178415bd9f6501
Sohail Ajmal <sohail.aj05@gmail.com>
2016-09-25 02:36:33 +0800

    add board class

---
+++ js/board.js
@@ -0,0 +1,24 @@
+class Board {
+
+  constructor(){
+
+    this.playerBoard = [];
+    this.opponentBoard = [];
+    this.player = {};
+    this.turn = "";
+  }
+
+  summon(card, side){
+
+  }
+
+  destroy(card){
+
+  }
+
+  draw(){
+    return player.deck.draw();
+  }
+
+
+}
